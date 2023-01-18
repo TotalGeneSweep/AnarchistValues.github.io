512e99b6ff0932c9c986e7ed43a7bb3c04ccf9d3
Gene <122518759+TotalGeneSweep@users.noreply.github.com>
2023-01-17 22:56:41 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -8,4 +8,31 @@ questions = [
             "scty": 5
         }
     },
+    {
+        "question": "I support regional unions, such as the European Union.",
+        "effect": {
+            "econ": -5,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "I support regional unions, such as the European Union.",
+        "effect": {
+            "econ": -5,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 5
+        }
+    },
+    {
+        "question": "I support regional unions, such as the European Union.",
+        "effect": {
+            "econ": -5,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 5
+        }
+    },
 ];
