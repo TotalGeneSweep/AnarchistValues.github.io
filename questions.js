8197881d442b5e9e9d626bf581f3834d2b3fa542
Gene <122518759+TotalGeneSweep@users.noreply.github.com>
2023-01-17 21:46:07 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -8,4 +8,13 @@ questions = [
             "scty": 0
         }
     },
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": -5
+        }
+    },
 ];
