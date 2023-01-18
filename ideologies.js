deec0222af767de43fc5acc2b5c43f7a2a75d375
Gene <122518759+TotalGeneSweep@users.noreply.github.com>
2023-01-17 22:47:55 -0700

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -2,10 +2,55 @@ ideologies = [
     {
         "name": "Anarcho-Communism",
         "stats": {
-            "tech": -55,
+            "tech": 45,
             "soc": 70,
             "phil": 60,
-            "econ": -80
+            "econ": 20
+        }
+    },
+    {
+        "name": "Ego-Communism",
+        "stats": {
+            "tech": 45,
+            "soc": 25,
+            "phil": 50,
+            "econ": 20
+        }
+    },
+    {
+        "name": "Mutualism",
+        "stats": {
+            "tech": 45,
+            "soc": 40,
+            "phil": 50,
+            "econ": 75
+        }
+    },
+    {
+        "name": "Post-Civ",
+        "stats": {
+            "tech": 70,
+            "soc": 35,
+            "phil": 40,
+            "econ": 55
+        }
+    },
+    {
+        "name": "Anti-Civ",
+        "stats": {
+            "tech": 25,
+            "soc": 40,
+            "phil": 50,
+            "econ": 75
+        }
+    },
+    {
+        "name": "Anarcho-Primitivism",
+        "stats": {
+            "tech": 0,
+            "soc": 75,
+            "phil": 65,
+            "econ": 40
         }
     },
 ];
