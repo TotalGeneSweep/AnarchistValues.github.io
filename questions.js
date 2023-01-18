e4458723526717c628c484626c436e0075203bcc
Gene <122518759+TotalGeneSweep@users.noreply.github.com>
2023-01-17 21:42:38 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,11 +1,1 @@
-questions = [
-    {
-        "question": "borker is very gay.",
-        "effect": {
-            "econ": 10,
-            "dipl": 0,
-            "govt": -5,
-            "scty": 0
-        }
-    }
-]
+
