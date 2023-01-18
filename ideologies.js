5999063a30bd4a8d3afb5ac82c2566be9796390a
Gene <122518759+TotalGeneSweep@users.noreply.github.com>
2023-01-17 22:55:56 -0700

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -80,4 +80,58 @@ ideologies = [
             "econ": 20
         }
     },
+    {
+        "name": "Egoism",
+        "stats": {
+            "tech": 45,
+            "soc": 0,
+            "phil": 65,
+            "econ": 55
+        }
+    },
+    {
+        "name": "Ego-Mutualism",
+        "stats": {
+            "tech": 45,
+            "soc": 20,
+            "phil": 65,
+            "econ": 75
+        }
+    },
+    {
+        "name": "Anarcho-Nihilism",
+        "stats": {
+            "tech": 65,
+            "soc": 75,
+            "phil": 20,
+            "econ": 45
+        }
+    },
+    {
+        "name": "Existentialist Anarchism",
+        "stats": {
+            "tech": 45,
+            "soc": 35,
+            "phil": 80,
+            "econ": 55
+        }
+    },
+    {
+        "name": "Anarcho-Individualism",
+        "stats": {
+            "tech": 45,
+            "soc": 20,
+            "phil": 45,
+            "econ": 65
+        }
+    },
+    {
+        "name": "Anarcho-Transhumanisn",
+        "stats": {
+            "tech": 0,
+            "soc": 60,
+            "phil": 50,
+            "econ": 45
+        }
+    },
 ];
