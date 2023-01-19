926881eac7b5a84772e0e7aa55f45fedb705e115
Gene <122518759+TotalGeneSweep@users.noreply.github.com>
2023-01-18 18:51:42 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,37 +2,37 @@ questions = [
     {
         "question": "I support regional unions, such as the European Union.",
         "effect": {
-            "econ": -5,
+            "econ": 10
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I support regional unions, such as the European Union.",
+        "effect": {
+            "econ": 0
             "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I support regional unions, such as the European Union.",
+        "effect": {
+            "econ": 0
+            "dipl": 0,
             "govt": 10,
-            "scty": 5
+            "scty": 0
         }
     },
     {
         "question": "I support regional unions, such as the European Union.",
         "effect": {
-            "econ": 5,
-            "dipl": -10,
-            "govt": -10,
-            "scty": -5
-        }
-    },
-    {
-        "question": "I support regional unions, such as the European Union.",
-        "effect": {
-            "econ": -5,
-            "dipl": 10,
-            "govt": -10,
-            "scty": 5
-        }
-    },
-    {
-        "question": "I support regional unions, such as the European Union.",
-        "effect": {
-            "econ": 5,
-            "dipl": -10,
-            "govt": 10,
-            "scty": -5
+            "econ": 0
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
         }
     },
 ];
